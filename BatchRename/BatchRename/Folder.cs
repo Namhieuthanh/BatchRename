6b4c83b2e520f4d4fa72eefeb712c8572b0db352
buitrihieu98 <51129013+buitrihieu98@users.noreply.github.com>
2019-10-31 17:57:03 +0700

    startButton+folders

--- BatchRename/BatchRename/Folder.cs
+++ BatchRename/BatchRename/Folder.cs
@@ -1,16 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace BatchRename
 {
-    class Folder
+    class Folder : INotifyPropertyChanged
     {
+        private string _newName;
+        public event PropertyChangedEventHandler PropertyChanged;
         public string Name { get; set; }
-        public string NewName { get; set; }
+        public string NewName
+        {
+            get { return _newName; }
+            set
+            {
+                _newName = value;
+                if (PropertyChanged != null)
+                {
+                    PropertyChanged(this, new PropertyChangedEventArgs("NewName"));
+                }
+            }
+        }
         public string Path { get; set; }
         public string Error { get; set; }
+        
     }
 }
